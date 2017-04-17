045e9d3822f3f543174dd7cadf70948a6c196d67
James Folberth (AWS) <jamesfolberth@gmail.com>
2017-04-17 15:53:39 +0000

    Minor improvements to start.sh
    - Only run sudo on the final jupyterhub command.  The OAuth client secret is still exported to ec2-users' environment variables, which should be fine.
    - Automatically find the EC2 instance's public hostname with `ec2-metadata --public-hostname`.  Then the user doesn't have to manually enter it in /srv/jupyterhub/env, which is nice.

--- jupyterhub/start.sh
+++ jupyterhub/start.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # Check that we got the vars we want
-vars="EC2_PUBLIC_DNS OAUTH_CLIENT_ID OAUTH_CLIENT_SECRET"
+vars="OAUTH_CLIENT_ID OAUTH_CLIENT_SECRET"
 for var in $vars; do
    if [ -z ${!var} ]; then # "indirect parameter expansion"
       echo "Error: Variable $var is not set" 1>&2
@@ -25,6 +25,16 @@ for var in $vars; do
    fi
 done
 
-export OAUTH_CALLBACK_URL=https://${EC2_PUBLIC_DNS}:8443/hub/oauth_callback
+# Get the public hostname
+export EC2_PUBLIC_HOSTNAME=`ec2-metadata --public-hostname | sed -ne 's/public-hostname: //p'`
+if [ -z $EC2_PUBLIC_HOSTNAME ]; then
+   echo "Error: Failed to get EC2 public hostname from `ec2-metadata`"
+   exit 1
+else
+   echo "Using EC2_PUBLIC_HOSTNAME=$EC2_PUBLIC_HOSTNAME"
+fi
+export OAUTH_CALLBACK_URL=https://${EC2_PUBLIC_HOSTNAME}:8443/hub/oauth_callback
 
-jupyterhub -f config.py
+# -E preserves environment variables (except not PATH, PYTHONPATH, etc.)
+jhub_bin=`which jupyterhub`
+sudo -E PYTHONPATH=$PYTHONPATH $jhub_bin -f config.py
