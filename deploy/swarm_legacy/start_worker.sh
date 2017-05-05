15f5c555a42e0f0e251b1cb09fe80e0b941eb07e
James Folberth <jamesfolberth@gmail.com>
2017-05-05 14:02:20 -0600

    Cleaned up deploy docs

--- deploy/swarm_legacy/start_worker.sh
+++ deploy/swarm_legacy/start_worker.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
-export DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"
+#JMF: this doesn't seem to have an effect... change OPTIONS in /etc/sysconfig/docker
+#export DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"
 
 if [ -z $1 ]; then
    echo "Error: Pass in local ip of consul container host as first argument"
