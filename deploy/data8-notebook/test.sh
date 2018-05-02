2fadb9634c7564274d1c9a8ab4ad4864aacd0ca9
James Folberth (EC2) <jamesfolberth@gmail.com>
2018-05-02 16:53:57 +0000

    Update data8-notebook Dockerfile to work with update jupyter/minimal-notebook; it pretty much just works now!

--- deploy/data8-notebook/test.sh
+++ deploy/data8-notebook/test.sh
@@ -2,8 +2,8 @@
 #docker run -it --rm -e USER=testuser -e USER_ID=1001 -e HOME=/home/testuser \
 #  --volume /home/testuser:/home/testuser \
 #  data8-notebook $@
-#docker run -it --rm -e USER=jamesfolberth -e USER_ID=1000 -e HOME=/home \
-#  --volume /mnt/nfs/home:/home\
-#  data8-notebook $@
-docker run -it --rm -e USER=testuser -e USER_ID=10000 -e HOME=/home \
+docker run -it --rm -e USER=jamesfolberth -e USER_ID=501 -e HOME=/home \
+  --volume /mnt/nfs/home:/home\
   data8-notebook $@
+#docker run -it --rm -e USER=testuser -e USER_ID=10000 -e HOME=/home \
+#  data8-notebook $@
