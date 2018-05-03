7763bff96a50c7eb2391e9f2a0de0af7b74c82a1
James Folberth <jamesfolberth@gmail.com>
2018-05-03 09:18:37 -0600

    Tag pulled jamesfolberth/data8-notebook as data8-notebook locally (for convenience)

--- deploy/data8-notebook/pull.sh
+++ deploy/data8-notebook/pull.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+# pull the latest image from hub.docker.com
 docker pull jamesfolberth/data8-notebook
+
+# tag it locally as data8-notebook
+docker tag jamesfolberth/data8-notebook data8-notebook
