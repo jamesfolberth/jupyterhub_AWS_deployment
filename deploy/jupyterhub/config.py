431f9802b8d0d6eb96fba90955aa06e80a6b40f0
James Folberth <jamesfolberth@gmail.com>
2018-05-31 16:32:15 -0600

    Remove bogus thing from config.py

--- deploy/jupyterhub/config.py
+++ deploy/jupyterhub/config.py
@@ -100,6 +100,5 @@ c.SystemUserSpawner.host_homedir_format_string = '/mnt/nfs/home/{username}'
 #     --NotebookApp.iopub_data_rate_limit=1000000000
 #     in $NOTEBOOK_ARGS, which was passed to the Jupyter server.
 c.DockerSpawner.extra_host_config = {'mem_limit': '1g',
-                                     'cpus': "1",
                                      }
 
