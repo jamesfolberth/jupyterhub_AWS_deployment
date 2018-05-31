a50d9dc214f8c3233c91c92c991d304ef58a9b2c
James Folberth <jamesfolberth@gmail.com>
2018-05-31 15:05:01 -0600

    Fix output dir name

--- deploy/static_site/update_s3.sh
+++ deploy/static_site/update_s3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . aws_env
 
-aws s3 sync jekyll_deploy s3://${S3_BUCKET_NAME} --delete
+aws s3 sync jekyll-output s3://${S3_BUCKET_NAME} --delete
 
 echo
 echo "S3 endpoint: ${S3_ENDPOINT_NAME}"
