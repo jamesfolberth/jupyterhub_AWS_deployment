a9d526c5500a7f97fd1599975d79e33a12b93511
James Folberth (EC2) <jamesfolberth@gmail.com>
2018-06-09 00:46:38 +0000

    Make copied repo notebook files readonly to force users to duplicate them in Jupyter

--- deploy/do_rsync.sh
+++ deploy/do_rsync.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo ./cp_to_users ~/repos/jupyterhub_AWS_deployment/notebooks/ example_notebooks
+export PYTHONPATH=$PYTHONPATH:/home/ec2-user/repos/jupyterhub_AWS_deployment/deploy/jupyterhub/python
+sudo env "PYTHONPATH=$PYTHONPATH" ./cp_to_users ~/repos/jupyterhub_AWS_deployment/notebooks/ example_notebooks
